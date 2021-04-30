d85ba093232e8a3bb9ea08208e5e3490753472a1
byk3nko <67236013+byk3nko@users.noreply.github.com>
2021-04-30 17:21:20 +0400

    Delete custom.lua

--- lua/autorun/client/custom.lua
+++
@@ -1,9 +0,0 @@
-local function addNotification( msgText, msgType, msgTime )
-
-	if IsValid( LocalPlayer() ) then
-		LocalPlayer():EmitSound( "shadowraze/shadowraze.wav", 100, math.random( 90,110 ), 0.5 )
-	end
-
-end
-
-notification.AddLegacy = addNotification
